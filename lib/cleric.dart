f489f87ce524ca485b5a811342a6f8916dedbd98
lcw <skcjddn@gmail.com>
2024-05-31 17:19:41 +0900

    - 클레릭의 능력을 구현했습니다.

--- lib/cleric.dart
+++ lib/cleric.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
   String name;
   int hp = 50;
@@ -5,6 +7,38 @@ class Cleric {
   final int maxHp = 50;
   final int maxMp = 10;
 
-  Cleric(this.name);
+  Cleric({
+    required this.name,
+    required this.hp,
+    required this.mp,
+  });
 
+  void selfAid() {
+    if ((mp - 5) >= 0) {
+      print('셀프에이드 성공!');
+      mp = mp - 5;
+      hp = maxHp;
+    } else {
+      print('MP가 부족합니다.');
+    }
+  }
+
+  int pray(int praySecond) {
+    print('기도하기!');
+    int randomPoint = Random().nextInt(3);
+    int recoveryMp = praySecond + randomPoint;
+    int totalMp = mp + recoveryMp;
+    int realRecoveryMp = 0;
+
+    if (maxMp <= totalMp) {
+      realRecoveryMp = maxMp - mp;
+      mp = maxMp;
+    } else {
+      mp = totalMp;
+      realRecoveryMp = recoveryMp;
+    }
+
+    print("회복된 MP: $realRecoveryMp");
+    return realRecoveryMp;
+  }
 }
