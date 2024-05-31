ef5a31d27fd389f3393cfa2f7bcd0cc5a7e4073b
lcw <skcjddn@gmail.com>
2024-05-31 17:20:39 +0900

    feat: 클레릭 인스턴스화

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:learn_dart_together/cleric.dart';
 
 void main() {
-  print('Hello world: !');
+  Cleric cleric = Cleric(name: "성직자",hp: 40,mp: 10);
+  cleric.selfAid();
+  cleric.pray(3);
 }
