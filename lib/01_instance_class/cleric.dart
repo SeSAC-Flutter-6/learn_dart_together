14e5848b615981b7fc09fe269122dcf711b05f97
Sangin Han <hugedreamhd@gmail.com>
2024-06-01 00:00:47 +0900

    클레릭의 pray 메서드 추가하고 기능을 구현했습니다. 클레릭 메서드 리턴값을 실제로 회복된 양으로 반환했습니다. test코드도 작성해보았습니다. 실제값이 랜덤값이어서 bool 값으로 범위를 비교해보았습니다.

--- lib/01_instance_class/cleric.dart
+++ lib/01_instance_class/cleric.dart
@@ -1,4 +1,6 @@
 //클레릭 클래스 생성
+import 'dart:math';
+
 class Cleric {
   //멤버변수
   String name;
@@ -25,23 +27,61 @@ class Cleric {
   //메서드 기능 -> 회복
   void selfAid() {
     if (mp >= 5) {
-      mp - 5;
+      //mp - 5와 mp -= 5의 차이점은?
+      mp -= 5;
       hp = maxHp;
-    };
-
-
-    print('$name이 회복기능을 사용했습니다.');
+    }
   }
+
+  //메서드 기능 -> pray / 회복기능
+  int pray(int sec) {
+    //회복된 mp가 저장되는 멤버변수
+    int recoveryMp = 0;
+    //0-2 숫자가 랜덤으로 생성되는 멤버변수
+    int number = Random().nextInt(3);
+    //현재 mp의 값
+    int initMp = this.mp;
+
+    if (sec >= 1) {
+      //mp를 회복했는데 최대(조건) mp 이상 채워지지 말아야한다면?
+      //실제 mp를 회복해야한다
+      recoveryMp = sec + number;
+      if (initMp + recoveryMp >= maxMp) {
+        recoveryMp = maxMp - initMp;
+      }
+    }
+    //실제 mp를 업데이트
+    this.mp = initMp + recoveryMp;
+    // if (mp < 9 && mp < maxMp) {
+    //   recoveryMp = sec + number;
+    // }
+
+    // if (mp <= 10) {
+    //   recoveryMp = mp += number;
+    // }
+
+    return recoveryMp;
+  }
+
+// } else if(recoveryMp >= maxMp) {
+// recoveryMp = maxMp;
+// }
+
 // 문제2
 // 성직자는 용사 처럼 이름과 HP를 가지고 있고, 추가로 마법을 사용하기 위한 MP를 가지고 있다.
 // 연습 1에서 작성한 내용이 없는 Cleric 클래스에 “이름", “HP”, “최대 HP”, “MP”, “최대 MP”를 속성으로 추가 하시오.
 // 또한 HP와 최대 HP는 정수로 초기치 50, MP와 최대 MP는 정수로 초기치 10으로 하고,
 // 최대 HP와 최대 MP는 상수 필드(프로퍼티에 기본값을 주면 생성자를 만들지 않아도 된다)로 선언 하시오.
 
-
 //  문제3
 //  성직자는 “셀프 에이드" 라는 마법을 사용할 수 있고, MP를 5소비하는 것으로 자신의 HP 를 최대 HP 까지 회복할 수 있다.
 //  연습 2 에 선언한 Cleric 클래스에 “selfAid()” 메소드를 추가 하시오.
 //  또한, 이 메소드는 인수가 없고, 리턴 값도 없다.
 
+// 문제4
+// 성직자는 “기도하기" (pray) 라는 행동을 취할 수 있고, 자신의 MP를 회복한다.
+// 회복량은 기도한 시간(초)에 랜덤하게 0 ~ 2포인트의 보정을 한 양이다 (3초 기도하면 회복량은 3 ~ 5 포인트).
+// 단, 최대 MP 보다 더 회복하는 것은 불가능 하다.
+// 연습 3에서 선언한 Cleric 클래스에 “pray()” 메소드를 추가하시오.
+// 이 메소드는 인수에 “기도할 시간(초)"를 지정할 수 있고, 리턴 값은 “실제로 회복된 MP 양" 을 반환한다.
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,26 @@ void main(List<String> arguments) {
   Slime slime1 = Slime(50, '슬라임A');
   Slime slime2 = Slime(40, '슬라임B');
 
-  Cleric cleric = Cleric('성직자', 50, 50);
+  Cleric cleric = Cleric('성직자', 50, 10);
 
   hero.attack();
   hero.run();
   hero.sleep();
   slime1.run();
   slime2.run();
+  cleric.selfAid();
+  print('현재 mp는 ${cleric.mp}');
+  cleric.selfAid();
+  print('현재 mp는 ${cleric.mp}');
+  print('${cleric.name}이 회복기능을 사용했습니다. 현재 mp는 ${cleric.mp} 입니다.');
+  print('2초만큼 기도했습니다. ${cleric.pray(2)} 만큼 mp가 회복됩니다');
+  print('현재 mp는 ${cleric.mp} 입니다');
 
+  // pray 메서드 테스트
+  int recoveredMp = cleric.pray(3);
+  print('실제로 회복된 MP 양: $recoveredMp');
+  print('현재 MP: ${cleric.mp}');
+  int recoveredMp2 = cleric.pray(3);
+  print('실제로 회복된 MP 양: $recoveredMp2');
+  print('현재 MP: ${cleric.mp}');
 }
