9a23291b748311792fb9b2b87ca8eedae95a1bf3
lcw <skcjddn@gmail.com>
2024-06-01 16:53:30 +0900

    style: 띄어쓰기 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:learn_dart_together/cleric.dart';
 
 void main() {
-  Cleric cleric = Cleric(name: "성직자",hp: 40,mp: 10);
+  Cleric cleric = Cleric(name: "성직자", hp: 40, mp: 10);
   cleric.selfAid();
   cleric.pray(3);
 }
