ec7674acb12b727c9d6324128b747c52f666a392
S-YUN <https://github.com/S-YUN>
2024-06-02 19:37:10 +0900

    test: cleric 생성자 수정에 따른 테스트코드 수정. 생성자 테스트와 메서드 테스트를 따로 그룹지음. 메서드 테스트의 경우 하나의 인스턴스만 생성하여 테스트

--- test/01_instance_class/cleric_test.dart
+++ test/01_instance_class/cleric_test.dart
@@ -4,16 +4,26 @@ import 'package:test/test.dart';
 void main() {
   late Cleric cleric;
 
-  group('Cleric 클래스 test', () {
+  group('Cleric constructor test', () {
+    test('(1): mp, hp 설정 x', () {
+      Cleric cleric = Cleric(name: "기본성직자");
+      expect([cleric.hp, cleric.mp], equals([50, 10]));
+    });
+    test('(2): mp, hp  max치보다 낮게 설정', () {
+      Cleric cleric = Cleric(name: "빈약한 성직자", hp: 25, mp: 5);
+      expect([cleric.hp, cleric.mp], equals([25, 5]));
+    });
+    test('(3): mp, hp  max치보다 높게 설정', () {
+      Cleric cleric = Cleric(name: "개쩌는 성직자", hp: 100000, mp: 10000);
+      expect([cleric.hp, cleric.mp], equals([cleric.maxHp, cleric.maxMp]));
+    });
+  });
+
+  group('Cleric method test', () {
     setUpAll(() {
       cleric = Cleric(name: '기본성직자');
     });
-
-    test('기본성직자 초기 상태 확인', () {
-      expect([cleric.hp, cleric.mp], equals([50, 10]));
-    });
-
-    test('selfAid() test1: 사용 시 5 차감, hp max회복 확인', () {
+    test('selfAid() - (1): 사용 시 5 차감, hp max회복 확인', () {
       //test를 위해 hp를 30으로 설정
       cleric.mp = cleric.maxMp;
       cleric.hp = 30;
@@ -21,14 +31,14 @@ void main() {
       expect([cleric.hp, cleric.mp], equals([cleric.maxHp, 5]));
     });
 
-    test('selfAid() test2: mp가 5 미만일 때 아무런 효과 없음', () {
+    test('selfAid() - (2): mp가 5 미만일 때 아무런 효과 없음', () {
       //test를 위해 mp를 2로 설정
       cleric.mp = 2;
       cleric.selfAid();
       expect([cleric.hp, cleric.mp], equals([cleric.hp, cleric.mp]));
     });
 
-    test('pray() test1: 현재 mp ==2, 3초 기도 시 3~5만큼의 mp 회복', () {
+    test('pray() - (1): 현재 mp ==2, 3초 기도 시 3~5만큼의 mp 회복', () {
       cleric.mp = 2;
       int praySeconds = 3;
       int prayResult = cleric.pray(praySeconds: praySeconds);
@@ -41,21 +51,21 @@ void main() {
           ]));
     });
 
-    test('pray() test2: 0초 기도 시 회복량 0', () {
+    test('pray() - (2): 0초 기도 시 회복량 0', () {
       int praySeconds = 0;
       int prayResult = cleric.pray(praySeconds: praySeconds);
 
       expect([prayResult, cleric.mp], equals([0, cleric.mp]));
     });
 
-    test('pray() test3: 현재 mp ==8, 3초 기도 시 mp는 maxMp만큼 회복. 회복량은 2', () {
+    test('pray() - (3): 현재 mp ==8, 3초 기도 시 mp는 maxMp만큼 회복. 회복량은 2', () {
       cleric.mp = 8;
       int praySeconds = 3;
       int prayResult = cleric.pray(praySeconds: praySeconds);
       expect([prayResult, cleric.mp], equals([2, cleric.maxMp]));
     });
 
-    test('pray() test4: 음수 초 기도 시 Exception throw', () {
+    test('pray() - (4): 음수 초 기도 시 Exception throw', () {
       int praySeconds = -3;
       expect(() => cleric.pray(praySeconds: praySeconds), throwsException);
     });
