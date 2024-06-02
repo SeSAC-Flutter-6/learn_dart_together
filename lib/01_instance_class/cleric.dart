289f87c7298dfec834acf52846ad93d9c6b0ac0d
S-YUN <https://github.com/S-YUN>
2024-06-02 18:55:24 +0900

    필요없는 파일 삭제

--- lib/01_instance_class/cleric.dart
+++ lib/01_instance_class/cleric.dart
@@ -10,28 +10,29 @@ class Cleric {
   Cleric({required this.name});
 
   void selfAid() {
-    //남은 mp가 5 이상이면 5만큼 차감
+    //남은 mp가 5 이상일때만 발동가능
     if (mp >= 5) {
       mp -= 5;
       hp = maxHp;
-    } else {
-      print('MP가 부족합니다');
     }
   }
 
   int pray({required int praySeconds}) {
-    //회복 가능한 mp
-    int recoverableMp = praySeconds + Random().nextInt(3);
-
-    //실제로 회복할 수 있는 mp => (recoverableMp + mp) 가 maxMp보다 크면 (maxMp - mp) , 작으면 recoverableMp
-    int actualRecoveredMp =
-        (recoverableMp+mp > maxMp) 
-          ? maxMp - mp 
-          : recoverableMp;
-    
-    //mp 회복
-    mp += actualRecoveredMp;
-    
-    return actualRecoveredMp;
+    if (praySeconds > 0) {
+      //회복 가능한 mp
+      final int recoverableMp = praySeconds + Random().nextInt(3);
+      //실제로 회복할 수 있는 mp => (recoverableMp + mp) 가 maxMp보다 크면 (maxMp - mp) , 작으면 recoverableMp
+      final int actualRecoveredMp =
+          (recoverableMp + mp > maxMp) ? maxMp - mp : recoverableMp;
+      //mp 회복
+      mp += actualRecoveredMp;
+      return actualRecoveredMp;
+    } else if (praySeconds == 0) {
+      //0초 기도 시 아무런 효과도 일어나지 않음 (정확히 명세하지 않음)
+      return 0;
+    } else {
+      //음수 초의 경우 Exception을 던진다.
+      throw Exception('기도 시간은 음수가 될 수 없음');
+    }
   }
 }
