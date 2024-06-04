d1dc8f37569a43da43d350a3b2c423fd9702c116
Jayone Lee <kenbugless@gmail.com>
2024-06-05 01:01:32 +0900

    [update]:Fix static keywords on class

--- lib/01_instance_class/cleric.dart
+++ lib/01_instance_class/cleric.dart
@@ -1,42 +1,49 @@
 import 'dart:math';
 
+void main() {
+  // Cleric cleric = Cleric(name: '성직자', hp: 50, mp: 10);
+  // cleric.selfAid();
+}
+
 class Cleric {
   String name;
   int hp;
   int mp;
   static const int maxHp = 50;
   static const int maxMp = 10;
+  static const int consumeMp = 5;
+  static const int randomMpValue = 3;
 
-  Cleric({required this.name, this.hp = maxHp, this.mp = maxMp}) {
-    hp = min(maxHp, hp);
-    mp = min(maxMp, mp);
+  Cleric({
+    required this.name,
+    this.hp = 50,
+    this.mp = 10,
+  }) {
+    hp = max(maxHp, hp);
+    mp = max(maxMp, mp);
   }
 
+//selfAid()는 사실, return을 쓰면 안되는데, 통과 X, 위에서 옮기는 방법을 적어주면 좋을 것 같다.
   void selfAid() {
-    //남은 mp가 5 이상일때만 발동가능
-    if (mp >= 5) {
-      mp -= 5;
+    //mp가 5소비가 되고, HP가 maxHp가 된다.
+    if (mp >= consumeMp) {
+      mp = mp - consumeMp;
       hp = maxHp;
+      print('mp 5를 소비하고, hp가 50으로 가득찹니다');
     }
   }
 
-  int pray({required int praySeconds}) {
-    if (praySeconds < 0) {
-      throw Exception('기도 시간은 음수가 될 수 없음');
+  int pray(int seconds) {
+    if (mp == maxMp) {
+      print('MP가 최대라 pray()를 사용할 필요가 없습니다.');
     }
-
-    if (praySeconds == 0) {
-      //0초 기도 시 아무런 효과도 일어나지 않음 (정확히 명세하지 않음)
-      return 0;
-    }
-
-    //회복 가능한 mp
-    final int recoverableMp = praySeconds + Random().nextInt(3);
-    //실제로 회복할 수 있는 mp => (recoverableMp + mp) 가 maxMp보다 크면 (maxMp - mp) , 작으면 recoverableMp
-    final int actualRecoveredMp =
-        (recoverableMp + mp > maxMp) ? maxMp - mp : recoverableMp;
-    //mp 회복
-    mp += actualRecoveredMp;
-    return actualRecoveredMp;
+    print('마나 회복(초당 0~2 random 증가) 중');
+    Random random = Random();
+    print('Random은 $random');
+    // 기도 시간에 비례하여 무작위로 마나를 회복
+    int recover = seconds + random.nextInt(randomMpValue); // 0에서 2 사이의 무작위 값 추가
+    int actualRecovery = min(recover, maxMp - mp); // 실제 회복량은 최대 MP를 초과하지 않도록 함
+    mp = min(mp + recover, maxMp); // 최대 MP를 초과하지 않도록 보장
+    return actualRecovery;
   }
 }
--- test/01_instance_class/cleric_test.dart
+++ test/01_instance_class/cleric_test.dart
@@ -1,16 +1,36 @@
 import 'package:learn_dart_together/01_instance_class/cleric.dart';
-import 'package:test/test.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
 
 void main() {
-  test('Cleric pray exception test', () {
-    final cleric = Cleric(name: 'name');
+  test('selfAid 테스트 해보기-hp가 부족한 상태임', () {
+    //given :테스트를 준비하는부분
+    final Cleric cleric = Cleric(name: '성직자', hp: 20, mp: 10);
+    //when: 테스트를 실행하는 부분
+    cleric.selfAid();
+    //then: 결과를 검증하는 부분
+    expect(cleric.hp, 50);
+    expect(cleric.mp, 5);
+  });
 
-    // 안 나는 케이스
-    expect(() => cleric.pray(praySeconds: 0), returnsNormally);
-    expect(() => cleric.pray(praySeconds: 1), returnsNormally);
+  test('selfAid 테스트 해보기2 - hp가 가득찬 상태임', () {
+    //given 테스트를 준비하는 부분
+    final Cleric cleric = Cleric(name: '성직자', hp: 50, mp: 10);
 
-    // 나는 케이스
-    expect(() => cleric.pray(praySeconds: -1), throwsException);
-    expect(() => cleric.pray(praySeconds: -2), throwsException);
+    //when 테스트를 실행하는 부분
+    cleric.selfAid();
+    //then: 결과를 검증하는 부분
+    expect(cleric.hp, 50);
+    expect(cleric.mp, 10);
+  });
+
+  test('pray 기도 테스트 - mp가 초당 잘 증가하는 지 여부 확인하기', () {
+    //given 테스트를 준비하는 부분
+    final Cleric cleric = Cleric(name: '성직자', hp: 50, mp: 7);
+    //when 테스트를 실행하는 부분
+    cleric.pray(4);
+    //then 결과를 검증하는 부분
+    expect(cleric.hp, 50);
+    expect(cleric.mp, 10);
   });
 }
