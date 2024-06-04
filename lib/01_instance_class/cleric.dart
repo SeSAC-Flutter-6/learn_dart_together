c78c865e8d444b06445c9e00fff9a46ad1876d15
S-YUN <https://github.com/S-YUN>
2024-06-04 14:57:22 +0900

    refactor: 코드 리뷰 후 리팩

--- lib/01_instance_class/cleric.dart
+++ lib/01_instance_class/cleric.dart
@@ -4,40 +4,38 @@ class Cleric {
   String name;
   int hp;
   int mp;
-  final int maxHp = 50;
-  final int maxMp = 10;
+  static const int maxHp = 50;
+  static const int maxMp = 10;
+  final int selfAidCost = 5;
 
-  Cleric({required this.name, this.hp = 50, this.mp = 10}) {
-    hp = hp > maxHp ? maxHp : hp;
-    mp = mp > maxMp ? maxMp : mp;
+  Cleric({required this.name, this.hp = maxHp, this.mp = maxMp}) {
+    hp = min(hp, maxHp);
+    mp = min(mp, maxMp);
   }
 
   void selfAid() {
-    //남은 mp가 5 이상일때만 발동가능
-    if (mp >= 5) {
-      mp -= 5;
+    //남은 mp가 cost 이상일때만 발동가능
+    if (mp >= selfAidCost) {
+      mp -= selfAidCost;
       hp = maxHp;
     }
   }
 
   int pray({required int praySeconds}) {
-    if (praySeconds > 0) {
-      //회복 가능한 mp
-      final int recoverableMp = praySeconds + Random().nextInt(3);
-      //실제로 회복할 수 있는 mp => (recoverableMp + mp) 가 maxMp보다 크면 (maxMp - mp) , 작으면 recoverableMp
-      final int actualRecoveredMp =
-          (recoverableMp + mp > maxMp) 
-            ? maxMp - mp 
-            : recoverableMp;
-      //mp 회복
-      mp += actualRecoveredMp;
-      return actualRecoveredMp;
-    } else if (praySeconds == 0) {
-      //0초 기도 시 아무런 효과도 일어나지 않음 (정확히 명세하지 않음)
-      return 0;
-    } else {
-      //음수 초의 경우 Exception을 던진다.
+    if (praySeconds < 0) {
       throw Exception('기도 시간은 음수가 될 수 없음');
     }
+    if (praySeconds == 0) {
+      //0초 기도 시 아무런 효과도 일어나지 않음 (정확히 명세하지 않음)
+      return 0;
+    }
+    //회복 가능한 mp
+    final int recoverableMp = praySeconds + Random().nextInt(3);
+    //실제로 회복할 수 있는 mp => (recoverableMp + mp) 가 maxMp보다 크면 (maxMp - mp) , 작으면 recoverableMp
+    final int actualRecoveredMp =
+        (recoverableMp + mp > maxMp) ? maxMp - mp : recoverableMp;
+    //mp 회복
+    mp += actualRecoveredMp;
+    return actualRecoveredMp;
   }
 }
--- test/01_instance_class/cleric_test.dart
+++ test/01_instance_class/cleric_test.dart
@@ -15,7 +15,7 @@ void main() {
     });
     test('(3): mp, hp  max치보다 높게 설정', () {
       Cleric cleric = Cleric(name: '개쩌는 성직자', hp: 100000, mp: 10000);
-      expect([cleric.hp, cleric.mp], equals([cleric.maxHp, cleric.maxMp]));
+      expect([cleric.hp, cleric.mp], equals([Cleric.maxHp, Cleric.maxMp]));
     });
   });
 
@@ -25,10 +25,10 @@ void main() {
     });
     test('selfAid() - (1): 사용 시 5 차감, hp max회복 확인', () {
       //test를 위해 hp를 30으로 설정
-      cleric.mp = cleric.maxMp;
+      cleric.mp = Cleric.maxMp;
       cleric.hp = 30;
       cleric.selfAid();
-      expect([cleric.hp, cleric.mp], equals([cleric.maxHp, 5]));
+      expect([cleric.hp, cleric.mp], equals([Cleric.maxHp, 5]));
     });
 
     test('selfAid() - (2): mp가 5 미만일 때 아무런 효과 없음', () {
@@ -62,12 +62,17 @@ void main() {
       cleric.mp = 8;
       int praySeconds = 3;
       int prayResult = cleric.pray(praySeconds: praySeconds);
-      expect([prayResult, cleric.mp], equals([2, cleric.maxMp]));
+      expect([prayResult, cleric.mp], equals([2, Cleric.maxMp]));
     });
 
     test('pray() - (4): 음수 초 기도 시 Exception throw', () {
-      int praySeconds = -3;
-      expect(() => cleric.pray(praySeconds: praySeconds), throwsException);
+      //경계선에 있는 애들로 테스트하는게 좋음
+      //0,1 테스트 성공 시 양수는 모두 성공한걸로 간주 -1.-2테스트 하면 그 밑으론 다 된걸로 간주
+      expect(() => cleric.pray(praySeconds: 1), returnsNormally);
+      expect(() => cleric.pray(praySeconds: 0), returnsNormally);
+      //-1.-2테스트 하면 음수는 모두 예외를 던진다고 간주
+      expect(() => cleric.pray(praySeconds: -1), throwsException);
+      expect(() => cleric.pray(praySeconds: -2), throwsException);
     });
   });
 }
