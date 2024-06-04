6e7c0982a2116b252b22049cc8783f7622723b8f
Kimdahee <dh980502@gmail.com>
2024-06-04 16:58:17 +0900

    240604 연습문제 1, 2

--- lib/01_instance_class/cleric.dart
+++ lib/01_instance_class/cleric.dart
@@ -2,15 +2,22 @@ import 'dart:math';
 
 class Cleric {
   String name;
-  int hp = 50;
-  int mp = 50;
-  final int hpMax = 50;
-  final int mpMax = 10;
+  int hp;
+  int mp;
+  static const int hpMax = 50;
+  static const int mpMax = 10;
+
   //static const int maxHp = 50;  이렇게 수정 후 생성자에서 사용하자.
-  //static : 미리 정해놓음. 정적
+  //static : 미리 정해 놓음. 정적
 
+  Cleric({required this.name, this.hp = hpMax, this.mp = mpMax}) {
+    hp = min(hp, hpMax);
+    mp = min(mp, mpMax);
+  }
 
-  Cleric({required this.name, required this.hp, required this.mp});
+  // Cleric({required this.name, int hp = hpMax, int mp = mpMax})
+  //     : hp = min(hp, hpMax),
+  //       mp = min(mp, mpMax);
 
   void selfAid() {
     if (mp >= 5) {
@@ -31,7 +38,3 @@ class Cleric {
   }
 }
 
-void main() {
-  Cleric cleric = Cleric(name: 'name', hp: 30, mp: 2);
-  print(cleric.pray(5));
-}
