93adb316611e67e6a9366d620f75f134b674d484
2021itb <syw7777@naver.com>
2024-06-05 00:18:39 +0900

    named constructor 사용

--- lib/01_instance_class/cleric.dart
+++ lib/01_instance_class/cleric.dart
@@ -7,24 +7,29 @@ void main() {
 class Cleric {
   String name;
   int hp;
+
   // static final int maxHp = 50;
   static const int maxHp = 50;
   int mp;
+
   // static final int maxMp = 10;
   static const int maxMp = 10;
 
   // 생성자
-  // Cleric({
-  //   required this.name,
-  //   required this.hp,
-  //   required this.mp,
-  // });
-
   Cleric({
+    required this.name,
+    required this.hp,
+    required this.mp,
+  });
+
+  Cleric.name({
     required this.name,
     this.hp = Cleric.maxHp,
     this.mp = Cleric.maxMp,
-  });
+  }) {
+    hp = min(hp, maxHp);
+    mp = min(mp, maxMp);
+  }
 
   void selfAid() {
     if (mp >= 5) {
@@ -38,7 +43,8 @@ class Cleric {
 
   int pray(int second) {
     int recoveredMp = Random().nextInt(3) + second;
-    int realRecoveredMp = recoveredMp + mp > Cleric.maxMp ? Cleric.maxMp - mp : recoveredMp;
+    int realRecoveredMp =
+        recoveredMp + mp > Cleric.maxMp ? Cleric.maxMp - mp : recoveredMp;
     mp += realRecoveredMp;
     print("mp +$realRecoveredMp 회복되었어요˘ᵕ˘ 현재 MP: $mp HP: $hp");
     return realRecoveredMp;
