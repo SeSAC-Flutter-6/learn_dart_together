2ba4af2f8538c8eb82121cc34d8d83f259ee5bfd
lcw <skcjddn@gmail.com>
2024-06-05 18:27:45 +0900

    refactor: test sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:learn_dart_together/cleric.dart';
 
+import 'package:learn_dart_together/person.dart';
+import 'package:learn_dart_together/wand.dart';
+import 'package:learn_dart_together/wizard.dart';
+
 void main() {
-  Cleric cleric = Cleric(name: "성직자", hp: 40, mp: 10);
+  /*Cleric cleric = Cleric(name: "성직자", hp: 40, mp: 10);
   cleric.selfAid();
-  cleric.pray(3);
+  cleric.pray(3);*/
+
+  Wand wand = Wand(name: "완드1", power: 100.0);
+  //Wand noNameWand = Wand(power: 100.0); //에러
+  Wizard wizard = Wizard(name: '세자', hp: 10, mp: 10, wand: null);
+  //wizard.wand = wand; // 에러
+
+  Person person = Person(name: '홍길동', birthYear: 1992);
+  print(person.age);
+
 }
