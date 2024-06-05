59b06dd3fb68408705fa0214eb46226c62b29731
Hunhee Lee <hunhee.dev@gmail.com>
2024-06-05 14:51:05 +0900

    [feat] Add exception handling to constructor

--- lib/01_instance_class/wand.dart
+++ lib/01_instance_class/wand.dart
@@ -5,5 +5,12 @@ class Wand {
   Wand({
     required this.name,
     required this.power
-  });
+  }) {
+    if (name.length < 3) {
+      throw Exception('지팡이의 이름은 3글자 이상이어야 합니다.');
+    }
+    if (power > 100 || power < 0.5) {
+      throw Exception('지팡이의 마력은 0.5 이상 100.0 이하여야 합니다.');
+    }
+  }
 }
--- lib/01_instance_class/wizard.dart
+++ lib/01_instance_class/wizard.dart
@@ -3,11 +3,25 @@ import 'package:learn_dart_together/01_instance_class/wand.dart';
 class Wizard {
   String name;
   int hp;
+  int mp;
   Wand? wand;
 
   Wizard({
     required this.name,
     required this.hp,
-    this.wand = null
-  });
+    required this.mp,
+    required this.wand
+  }) {
+    if (wand == null) {
+      throw Exception('마법사의 지팡이는 null 일 수 없습니다.');
+    }
+
+    if (mp < 0) {
+      throw Exception('마법사의 MP는 0 이상이어야 합니다.');
+    }
+
+    if (hp < 0) {
+      hp = 0;
+    }
+  }
 }
