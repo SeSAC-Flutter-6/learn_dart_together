e7452f4d235e449e81c7e566c4ef2786dbaaca35
Hunhee Lee <hunhee.dev@gmail.com>
2024-06-05 15:49:19 +0900

    [refactor] replace access modifier to private for hp property
    - Add getter and setter for hp property to manage access

--- lib/01_instance_class/wizard.dart
+++ lib/01_instance_class/wizard.dart
@@ -1,17 +1,27 @@
+import 'dart:math';
 import 'package:learn_dart_together/01_instance_class/wand.dart';
 
 class Wizard {
   String name;
-  int hp;
+  int _hp;
   int mp;
   Wand? wand;
 
+  int get hp => _hp;
+  set hp(int value) {
+    _hp = max(0, value);
+  }
+
   Wizard({
     required this.name,
-    required this.hp,
+    required int hp,
     required this.mp,
     required this.wand
-  }) {
+  }) : _hp = hp {
+    if (name.length < 3) {
+      throw Exception('마법사의 이름은 3글자 이상이어야 합니다.');
+    }
+
     if (wand == null) {
       throw Exception('마법사의 지팡이는 null 일 수 없습니다.');
     }
@@ -20,8 +30,8 @@ class Wizard {
       throw Exception('마법사의 MP는 0 이상이어야 합니다.');
     }
 
-    if (hp < 0) {
-      hp = 0;
+    if (_hp < 0) {
+      _hp = 0;
     }
   }
 }
