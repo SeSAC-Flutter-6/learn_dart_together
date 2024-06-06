e3cce08236467e8a6a1b4d057e62224683a98b7e
Hunhee Lee <hunhee.dev@gmail.com>
2024-06-06 23:32:30 +0900

    [refactor] Move static current year calculation to external function

--- lib/01_instance_class/person.dart
+++ lib/01_instance_class/person.dart
@@ -4,19 +4,26 @@ class Person {
   final String name;
   final int birthYear;
 
-  static final now = DateTime.now();
-  static final nowYear = now.year;
-
   // 나이는 올해년도에서 출생년도를 뺀 값을 리턴한다.
-  int get age => nowYear - birthYear;
+  int get age {
+    final nowYear = getNowYear();
+    return nowYear - birthYear;
+  }
 
   // 이름과 출생년도를 생성자로부터 주입 받는다
   Person({
     required this.name,
     required this.birthYear
   }) {
+    final nowYear = getNowYear();
     if (birthYear > nowYear) {
       throw Exception('출생년도는 현재 연도보다 이전이어야 합니다.');
     }
   }
+}
+
+int getNowYear() {
+  final now = DateTime.now();
+  final nowYear = now.year;
+  return nowYear;
 }
