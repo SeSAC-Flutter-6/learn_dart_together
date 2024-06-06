b62db0ac2270a0538a26b8ddffb776b68a32d7bc
S-YUN <https://github.com/S-YUN>
2024-06-06 17:09:45 +0900

    refactor: wand 인스턴스 생성시, 생성자에서도 세터를 호출하여 유효성 검사하게 수정

--- lib/0605/wand.dart
+++ lib/0605/wand.dart
@@ -6,19 +6,24 @@ class Wand {
     required String name,
     required double power,
   })  : _name = name,
-        _power = power;
+        _power = power {
+    //세터를 호출하여 생성자 만들때도 유효성 검사
+    this.name = name;
+    this.power = power;
+  }
 
   String get wandName => _name;
   double get wandPower => _power;
 
-  set name(String name){
-    if (name.length<3){
+  set name(String name) {
+    if (name.length < 3) {
       throw Exception('지팡이의 이름은 3문자 이상이어야 합니다');
     }
     _name = name;
   }
-   set power(double power){
-    if (power>100.0||power<0.5){
+
+  set power(double power) {
+    if (power > 100.0 || power < 0.5) {
       throw Exception('지팡이의 마력 범위는 0.5이상, 100.0이하입니다');
     }
     _power = power;
