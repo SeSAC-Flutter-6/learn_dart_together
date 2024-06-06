c8f706bb305969f8f6576029c793b95e603f733b
2021itb <syw7777@naver.com>
2024-06-06 22:01:18 +0900

    test 코드 한개로 수정

--- test/01_instance_class/person_test.dart
+++ test/01_instance_class/person_test.dart
@@ -3,11 +3,9 @@ import 'package:learn_dart_together/01_instance_class/person.dart';
 
 void main() {
   test('Person 클래스의 생성자가 예외를 발생시키는지 테스트', () {
-    test('태어난 년도가 현재 년도보다 클 경우', () {
-      final nowYear = DateTime.now().year;
-      final futureYear = nowYear - 1;
-      expect(() => Person('John', futureYear), throwsException);
-    });
+    final nowYear = DateTime.now().year;
+    final futureYear = nowYear - 1; //태어난 년도가 현재 년도보다 클 경우
+    expect(() => Person('John', futureYear), throwsException);
   });
 
   test('Person 클래스의 나이 계산이 올바른지 테스트', () {
