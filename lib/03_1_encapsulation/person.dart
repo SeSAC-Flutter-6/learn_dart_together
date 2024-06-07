0ae4f89b98c231e9a4119b0d26975ce382a7b3a8
S-YUN <https://github.com/S-YUN>
2024-06-07 12:16:03 +0900

    refactor: age 프로퍼티는 간결히, 생성자에서 출생년도 유효성 검사

--- lib/03_1_encapsulation/person.dart
+++ lib/03_1_encapsulation/person.dart
@@ -4,18 +4,14 @@ class Person {
 
   Person({required String name, required int birthYear})
       : _name = name,
-        _birthYear = birthYear;
+        _birthYear = birthYear {
+    if (this.birthYear > DateTime.now().year) {
+      throw Exception(
+          '출생년도가 올해년도보다 미래일 수 없음 (출생년도 : $_birthYear, 올해년도 : ${DateTime.now().year})');
+    }
+  }
 
   String get name => _name;
   int get birthYear => _birthYear;
-
-  int get age {
-    final int currentYear = DateTime.now().year;
-    final int age = currentYear - _birthYear;
-    if (age < 0) {
-      throw Exception(
-          '출생년도가 올해년도보다 미래일 수 없음 (출생년도 : $_birthYear, 올해년도 : $currentYear)');
-    }
-    return age;
-  }
+  int get age => DateTime.now().year - _birthYear;
 }
