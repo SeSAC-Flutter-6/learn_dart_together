febb3245d35c2647de102674c47c147c9d2df7b7
Kimdahee <dh980502@gmail.com>
2024-06-07 17:15:02 +0900

    feat : point, rectangle 클래스 작성

---
+++ lib/05_inheritance/point/rectangle.dart
@@ -0,0 +1,16 @@
+import 'package:learn_dart_together/05_inheritance/point/point.dart';
+
+class Rectangle {
+  int width;
+  int height;
+  Point topLeftPointer;
+
+  int get area => width * height;
+
+  int get perimeter => 2 * (width + height);
+
+  Rectangle(
+      {required this.width,
+      required this.height,
+      required this.topLeftPointer});
+}
