fa0434504b761130ceae9600d52124820185f79a
lcw <skcjddn@gmail.com>
2024-06-09 23:04:30 +0900

    refactor: poisonAttackCount 캡슐화

--- lib/inheritance/slime/poison_slime.dart
+++ lib/inheritance/slime/poison_slime.dart
@@ -3,18 +3,21 @@ import 'package:learn_dart_together/inheritance/slime/slime.dart';
 import '../../hero.dart';
 
 class PoisonSlime extends Slime {
-   static int poisonAttackCount = 5;
+  int _poisonAttackCount = 5;
 
-  PoisonSlime(super.suffix);
+  int get poisonAttackCount => _poisonAttackCount;
+
+  PoisonSlime(super.suffix, {poisonAttackCount = 5})
+      : _poisonAttackCount = poisonAttackCount;
 
   @override
   void attack(Hero hero) {
     super.attack(hero);
-    if (poisonAttackCount > 0) {
+    if (_poisonAttackCount > 0) {
       print('추가로, 독 포자를 살포했다!');
       print('${hero.hp ~/ 5}포인트의 데미지');
       hero.hp -= hero.hp ~/ 5;
-      poisonAttackCount -= 1;
+      _poisonAttackCount -= 1;
     }
   }
 }
