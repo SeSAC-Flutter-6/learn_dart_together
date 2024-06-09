9bf104c40ee8fe8e33b3e292bbe6310fd0e4dd81
Kimdahee <dh980502@gmail.com>
2024-06-09 17:33:28 +0900

    fix : 오타 수정, attackCount private으로 변경

--- lib/05_inheritance/poison_slime.dart
+++ lib/05_inheritance/poison_slime.dart
@@ -2,21 +2,24 @@ import 'package:learn_dart_together/01_instance_class/hero.dart';
 import 'package:learn_dart_together/05_inheritance/slime.dart';
 
 class PoisonSlime extends Slime {
-  int attackCount = 5;
+  int _attackCount = 5;
   int plusDamage = 0;
 
   PoisonSlime(super.suffix);
 
+  int get attackCount => _attackCount;
+
   @override
   void attack(Hero hero) {
     super.attack(hero);
-    plusDamage = 0;
-    if (attackCount > 0) {
+    if (_attackCount > 0) {
       print('추가로, 독 포자를 살포했다.!');
       plusDamage = hero.hp ~/ 5;
       hero.hp -= plusDamage;
       print('$plusDamage 포인트의 데미지');
-      attackCount -= 1;
+      _attackCount -= 1;
+    } else {
+      plusDamage = 0;
     }
   }
 
--- lib/05_inheritance/vehicle/car.dart
+++ lib/05_inheritance/vehicle/car.dart
@@ -3,7 +3,7 @@ import 'package:learn_dart_together/05_inheritance/vehicle/vehicle.dart';
 class Car extends Vehicle {
   Car({required super.maker, required super.model, required super.year});
 
-  void openSunRook() {
+  void openSunRoof() {
     print('선루프를 열었습니다.');
   }
 }
