94e861e538487456619c9c3f5328bcae9566677c
Sangin Han <hugedreamhd@gmail.com>
2024-06-10 17:39:20 +0900

    fix : point 테스트파일을 속성으로 하지 않고 개별 클래스로 해봄

--- lib/04_inheitance/point.dart
+++ lib/04_inheitance/point.dart
@@ -10,18 +10,20 @@ class Point {
 }
 
 //Rectangle 클래스를 만들고 width, height, top-left 포인터와 같은 속성을 포함합니다.
-class Rectangle extends Point {
+class Rectangle {
   double width; //넓이
   double height; //높이
+  Point topLeft; // 왼쪽 위에 점
 
   //Rectangle 클래스의 면적 및 둘레를 알려주는 프로퍼티를 추가합니다.
+  //double get point => super.x * super.y;// topleft 포인트 위치 계산
   double get area => width * height; //면적계산 프로퍼티
   double get parameter => 2 * (width + height); //둘레계산 프로퍼티
 
   Rectangle(
-    super.x,
-    super.y,
+
     this.width,
     this.height,
+    this.topLeft,
   );
 }
--- test/01_instance_class/point.dart
+++ test/01_instance_class/point.dart
@@ -5,22 +5,27 @@ import 'package:test/test.dart';
 void main() {
   test('Rectangle이 point를 상속받아 올바른 top_left 좌표를 가지는지 테스트',() {
     //준비(given)
-    Rectangle rectangle = Rectangle(2.0, 4.0, 6.0, 8.0);
-
+    Point topLeft = Point(3.0, 4.0);
+    Rectangle rectangle = Rectangle(10.0, 20.0, topLeft);
     //검증(then)
-    expect(rectangle.x ,equals(2.0));
-    expect(rectangle.y ,equals(4.0));
+
     expect(rectangle.width ,equals(6.0));
     expect(rectangle.height ,equals(8.0));
+    expect(rectangle.topLeft.x, equals(3.0));
+    expect(rectangle.topLeft.y, equals(4.0));
+
+
   });
   
   test('Rectangle이 둘레와 면적을 제대로 표현하는지 테스트', () {
     //준비(given)
-    Rectangle rectangle = Rectangle(0, 0, 4.0, 5.0);
+    Point topLeft = Point(3.0, 4.0);
+    Rectangle rectangle = Rectangle(4.0, 5.0, topLeft);
 
     //검증(then)
     expect(rectangle.area, equals(20.0));
     expect(rectangle.parameter, equals(18));
+
   });
 }
 
