44a71ef7498c89fa4d7ce1e881590e633393c11f
Sangin Han <hugedreamhd@gmail.com>
2024-06-10 22:54:19 +0900

    feat : book 클래스 수정, book클래스 uml 작성, vehicle uml 수정

--- lib/04_inheitance/library.dart
+++ lib/04_inheitance/library.dart
@@ -7,32 +7,74 @@ class Book {
   String title;
   String author;
   String genre;
+  String bookId;
 
   Book(
     this.title,
     this.author,
     this.genre,
+    this.bookId,
   );
+
+  void openBook() {
+    print('책을 뜯었습니다.');
+  }
+
+  void checkRefund() {
+    print('오픈시 환불불가');
+  }
 }
 
 class Fiction extends Book {
   String subgenre; //하위 장르
+  String creativity; //창의성
+  String imagination; //상상력
 
   Fiction(
     super.title,
     super.author,
     super.genre,
+    super.bookId,
     this.subgenre,
+    this.creativity,
+    this.imagination,
   );
+
+  @override
+  void openBook() {
+    print('책을 뜯었습니다.');
+  }
+
+  @override
+  void checkRefund() {
+    print('오픈시 환불불가');
+  }
 }
 
 class NonFiction extends Book {
   String subject; //주제
+  String reality; // 실제
+  String infoTransfer; //정보전달
+  String eduValue; // 교육가치
 
   NonFiction(
     super.title,
     super.author,
     super.genre,
+    super.bookId,
     this.subject,
+    this.reality,
+    this.infoTransfer,
+    this.eduValue,
   );
+
+  @override
+  void openBook() {
+    print('책을 뜯었습니다.');
+  }
+
+  @override
+  void checkRefund() {
+    print('오픈시 환불불가');
+  }
 }
