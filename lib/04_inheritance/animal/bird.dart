893111f6e4b456c0d8df57184dfcfe3ee6033cd6
S-YUN <https://github.com/S-YUN>
2024-06-11 10:55:06 +0900

    feat: bird 클래스 착륙하기 구현

--- lib/04_inheritance/animal/bird.dart
+++ lib/04_inheritance/animal/bird.dart
@@ -3,7 +3,9 @@ import 'package:learn_dart_together/04_inheritance/animal/animal.dart';
 class Bird extends Animal {
   bool _isFlying = false;
 
-  Bird({required super.sound}) : super(type: AnimalType.bird);
+  Bird({
+    required super.sound,
+  }) : super(type: AnimalType.bird);
 
   void fly() {
     if (_isFlying) {
@@ -11,5 +13,13 @@ class Bird extends Animal {
     }
     _isFlying = true;
     print('비행 시작!');
+  }
+
+  void land() {
+    if (!_isFlying) {
+      print('비행 중이 아니므로 착륙할 수 없습니다');
+    }
+    _isFlying = false;
+    print('착륙합니다');
   }
 }
