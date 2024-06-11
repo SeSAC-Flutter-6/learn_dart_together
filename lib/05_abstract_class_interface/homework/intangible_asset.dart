2e70520baf0d2746de27b70c8b932ece6c45527d
lcw <skcjddn@gmail.com>
2024-06-11 15:59:16 +0900

    feat: Add IntangibleAsset abstract class

---
+++ lib/05_abstract_class_interface/homework/intangible_asset.dart
@@ -0,0 +1,6 @@
+import 'package:learn_dart_together/05_abstract_class_interface/homework/asset.dart';
+
+abstract class IntangibleAsset extends Asset { //'나' class
+  @override
+  void checkAssetType() => print('무형자산 입니다.');
+}
