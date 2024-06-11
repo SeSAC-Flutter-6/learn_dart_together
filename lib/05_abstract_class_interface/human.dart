cb3dd94e9e91d249229ddb97991dd3255bfe42f4
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-06-11 13:43:55 +0900

    추상클래스, 인터페이스

---
+++ lib/05_abstract_class_interface/human.dart
@@ -0,0 +1,17 @@
+// 추상클래스 extends
+abstract class Human {
+  void speak();
+}
+
+// 인터페이스, 프로토콜 implements
+abstract interface class Human2 {
+
+}
+
+class Man implements Human {
+  @override
+  void speak() {
+    // TODO: implement speak
+  }
+
+}
