69175a8895737e63932aa3583562ef6cbba41f04
Calia <eqqmayo@gmail.com>
2024-06-11 16:06:13 +0900

    feat: 추상클래스, 인터페이스 연습문제

---
+++ lib/05_abstract_class_interface/asset/computer.dart
@@ -0,0 +1,13 @@
+import 'package:learn_dart_together/05_abstract_class_interface/asset/tangible_asset.dart';
+
+class Computer extends TangibleAsset {
+  String makerName;
+
+  Computer({
+    required super.name,
+    required super.price,
+    required super.color,
+    required super.weight,
+    required this.makerName, 
+  });
+}
