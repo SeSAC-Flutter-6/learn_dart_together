e75de851f884a21e666fedf7373e4ff3154e3c99
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-06-12 12:15:55 +0900

    자산 과제 수정

--- lib/05_abstract_class_interface/exam/tangible_asset.dart
+++ lib/05_abstract_class_interface/exam/tangible_asset.dart
@@ -3,18 +3,14 @@ import 'package:learn_dart_together/05_abstract_class_interface/exam/thing.dart'
 
 abstract class TangibleAsset extends Asset implements Thing {
   String color;
-  double _weight;
 
   @override
-  double get weight => _weight;
-
-  @override
-  set weight(double value) => _weight = value;
+  double weight;
 
   TangibleAsset({
     required super.name,
     required super.price,
     required this.color,
-    required double weight,
-  }) : _weight = weight;
+    required this.weight,
+  });
 }
