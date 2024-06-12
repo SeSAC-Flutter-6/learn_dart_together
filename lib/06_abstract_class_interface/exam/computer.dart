13ec26d3b07e917426a6ac6287809d1682202d0e
Sangin Han <hugedreamhd@gmail.com>
2024-06-12 23:32:11 +0900

    feat : starcraft.puml 작성

---
+++ lib/06_abstract_class_interface/exam/computer.dart
@@ -0,0 +1,15 @@
+class Computer {
+  String name;
+  int price;
+  String color;
+  String isbn;
+
+  Computer ({
+    required this.name,
+    required this.price,
+    required this.color,
+    required this.isbn,
+
+  });
+
+}
