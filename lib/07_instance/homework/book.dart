f0ae2440c6dba8b8f5f7c0a0892d89038f9bad20
lcw <skcjddn@gmail.com>
2024-06-13 17:06:43 +0900

    feat: Add book

---
+++ lib/07_instance/homework/book.dart
@@ -0,0 +1,74 @@
+/*
+다음 동작을 할 수 있도록 Book 클래스를 수정하시오.
+
+제목과 출간일(2024-01-01)이 같으면 같은 책으로 판단한다
+Book 인스턴스를 담고 있는 컬렉션에 대해 sort() 를 수행하여 출간일이 신상 순서대로 정렬한다.
+deep copy 를 위한 copyWith() 메서드를 제공한다
+*/
+
+class Book {
+  String title;
+  String comment;
+  DateTime publishDate;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Book &&
+          runtimeType == other.runtimeType &&
+          title == other.title &&
+          //publishDate.toString().substring(0,10) == other.publishDate.toString().substring(0,10);
+          publishDate.year == other.publishDate.year &&
+          publishDate.month == other.publishDate.month &&
+          publishDate.day == other.publishDate.day;
+
+  @override
+  int get hashCode => title.hashCode ^ publishDate.hashCode;
+
+  Book({
+    required this.title,
+    required this.comment,
+    DateTime? publishDate,
+  }) : publishDate = publishDate ?? DateTime.now();
+
+  @override
+  String toString() {
+    return 'Book{title: $title, comment: $comment, publishDate: $publishDate}';
+  }
+
+  Book copyWith({
+    String? title,
+    String? comment,
+    DateTime? publishDate,
+  }) {
+    return Book(
+      title: title ?? this.title,
+      comment: comment ?? this.comment,
+      publishDate: publishDate ?? this.publishDate,
+    );
+  }
+}
+
+void main() {
+  Book book = Book(title: '자바의정석', comment: '어렵')
+  final books = [
+    Book(
+      title: '플러터마스터되기',
+      comment: '기본서',
+      publishDate: DateTime(2024, 06, 13),
+    ),
+    Book(
+      title: '플러터마스터되기',
+      comment: '개정판1',
+      publishDate: DateTime(2025, 06, 14),
+    ),
+    Book(
+      title: '플러터마스터되기',
+      comment: '개정판2',
+      publishDate: DateTime(2025, 12, 25),
+    ),
+  ];
+
+  books.sort((a, b) => a.publishDate.compareTo(b.publishDate));
+  print(books);
+}
