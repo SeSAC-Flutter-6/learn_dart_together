b5b0aaa26cb19765eb42399e32c8d08455e7a6f7
S-YUN <https://github.com/S-YUN>
2024-06-13 13:36:11 +0900

    feat: Book 클래스 작성

---
+++ lib/07_instance_operator/book.dart
@@ -0,0 +1,11 @@
+class Book {
+  String title;
+  DateTime publishDate;
+  String comment;
+
+  Book({
+    required this.title,
+    required this.comment,
+    DateTime? publishDate,
+  }) : publishDate = publishDate ?? DateTime.now();
+}
