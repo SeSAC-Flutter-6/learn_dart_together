1381a18c92aeee289c3da4e5f6b4e424e3e45c71
Kimdahee <dh980502@gmail.com>
2024-06-14 18:17:13 +0900

    240614 제네릭, 열거형, 문자열 조작 수업중 코드 추가

--- lib/09_generic_enum/generic.dart
+++ lib/09_generic_enum/generic.dart
@@ -9,3 +9,13 @@ class StrongBox<E> {
     return _data;
   }
 }
+
+void main() {
+  StrongBox<String> strongBox = StrongBox();
+  StrongBox<int> strongBox1 = StrongBox();
+  strongBox.put('present');
+  strongBox1.put(2);
+
+  String string = 'name';
+  string.toUpperCase();
+}
