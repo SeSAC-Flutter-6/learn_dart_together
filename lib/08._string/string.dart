18c915134995f6c9200d0d3950e75e1610fb046e
2021itb <syw7777@naver.com>
2024-06-14 20:34:10 +0900

    제네릭, 열거형 연습문제1, 2, 문자열 조작 실습 1, 2

---
+++ lib/08._string/string.dart
@@ -0,0 +1,7 @@
+void main() {
+  String greeting = 'Hello';
+
+  print(greeting.replaceAll('H', 'J')); //String은 불변이다
+  print(greeting); //Hello
+
+}
---
+++ lib/08._string/strong_box.dart
@@ -0,0 +1,60 @@
+import 'package:collection/collection.dart';
+class StrongBox <T> {
+  final KeyType _keyType;
+  final int _maxAttempts;
+
+  int get maxAttempts => _maxAttempts;
+  int _attemps = 0;
+  T? _item;
+
+  void put(T item) {
+     _item ??= item;
+  }
+  T? get() {
+    if(_attemps < _maxAttempts){
+      _attemps++;
+      return null;
+    }else {
+      T? temp = _item;
+      _item = null;
+      return temp;
+    }
+  }
+
+  StrongBox(this._keyType)
+      : _maxAttempts = _maxAttemptsForType(_keyType);
+
+  static int _maxAttemptsForType(KeyType keyType){
+    switch(keyType) {
+
+      case KeyType.padlock:
+        return 1024;
+        // TODO: Handle this case.
+      case KeyType.button:
+        return 10000;
+        // TODO: Handle this case.
+      case KeyType.dial:
+        return 30000;
+        // TODO: Handle this case.
+      case KeyType.finger:
+        return 1000000;
+        // TODO: Handle this case.
+    }
+  }
+}
+enum KeyType {
+  padlock,
+  button,
+  dial,
+  finger,
+}
+
+void main(){
+  StrongBox<String> strongBox = StrongBox <String> (KeyType.dial);
+  strongBox.put('gold');
+  for(int i = 0 ; i <strongBox.maxAttempts ; i++){
+    print(strongBox.get()); // null
+  }
+  // 사용 횟수가 허용 범위를 초과한 경우 열림
+  print(strongBox.get()); // gold
+}
