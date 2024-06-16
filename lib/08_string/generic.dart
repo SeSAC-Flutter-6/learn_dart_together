d3c54e13f9d831f3b0eef838e36aff630a20eeb1
hugedreamhd <hugedreamhd@gmail.com>
2024-06-16 22:57:23 +0900

    feat : 240614 과제 : 게터에 있던 시도 카운트 체크를 메서드를 따로 구분하고 생성자에서 setMaxCount를 체크해서 각 타입이 불릴때 설정값이 한번만 불린다.

--- lib/08_string/generic.dart
+++ lib/08_string/generic.dart
@@ -10,51 +10,57 @@ class StrongBox<T> {
   T? _gold;
   final KeyType _keyType;
   int _count = 0; //열쇠 시도 횟수
-  int _maxCount = 0; //최대 시도 횟수
+  final int _maxCount; //최대 시도 횟수
 
 //열쇠의 종류를 받는 생성자
   StrongBox({
     required KeyType keyType,
-  }) : _keyType = keyType;
+  })  : _keyType = keyType,
+        _maxCount = _setMaxCount(keyType);
 
   void put(T gold) {
     _gold = gold;
   }
 
-  T? get() {
+
+  // 시도 횟수 증가 메서드
+  void attempt() {
     _count++;
-    switch (_keyType) {
+    if(_count > _maxCount) {
+      throw Exception('최대 시도 횟수를 초과했습니다');
+    }
+  }
+
+  //열쇠 종류에 따른 최대 시도 횟수 setting
+  static int _setMaxCount(KeyType keyType) {
+    switch (keyType) {
       case KeyType.padlock:
-        _maxCount = 1024;
-        if (_count >= _maxCount) {
-          return _gold;
-        } else {
-          return null;
+        {
+          return 1024;
         }
-
       case KeyType.button:
-        _maxCount = 10000;
-        if (_count >= _maxCount) {
-          return _gold;
-        } else {
-          return null;
+        {
+          return 10000;
         }
-
       case KeyType.dial:
-        _maxCount = 30000;
-        if (_count >= _maxCount) {
-          return _gold;
-        } else {
-          return null;
+        {
+          return 30000;
         }
-
       case KeyType.finger:
-        _maxCount = 1000000;
-        if (_count >= _maxCount) {
-          return _gold;
-        } else {
-          return null;
+        {
+          return 1000000;
         }
+      default:
+        throw Exception('열쇠 유형을 확인해 주세요');
+    }
+  }
+
+  //금고 내용물 반환 메서드
+  T? get() {
+    if (_count >= _maxCount) {
+      return _gold;
+    } else {
+      return null; //잠금 해제 전 null 반환
     }
   }
 }
