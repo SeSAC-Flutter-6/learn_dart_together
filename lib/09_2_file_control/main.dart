aa3f42ca1a86d3295963ab44a79066d1b4b17b9e
Hunhee Lee <hunhee.dev@gmail.com>
2024-06-18 20:15:43 +0900

    file copy 연습문제 풀이

---
+++ lib/09_2_file_control/main.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  copy('lib/09_2_file_control/testFile.txt', 'lib/09_2_file_control/copiedTestFile.txt');
+}
+
+void copy(String source, String target) {
+  final File originalFile = File(source);
+  final String text = originalFile.readAsStringSync();
+
+  final _ = File(target).writeAsStringSync(text);
+}
