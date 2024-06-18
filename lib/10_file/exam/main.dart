75b6e35fe5c80fb4e157bf2717953fc35228ef83
lcw <skcjddn@gmail.com>
2024-06-18 21:53:17 +0900

    style: 주석 위치 변경

--- lib/10_file/exam/main.dart
+++ lib/10_file/exam/main.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 
 void main() {
   final file = File('save.txt'); // 파일 열기
-  file.writeAsStringSync('copy 테스트 입니다.'); // 내용 쓰기(기존에 내용이 있으면 지워지고 새로 써짐)
-  final text = file
-      .readAsStringSync(); // 파일 없을시 PathNotFoundException: Cannot open file 에러 발생
+
+  // 내용 쓰기(기존에 내용이 있으면 지워지고 새로 써짐)
+  file.writeAsStringSync('copy 테스트 입니다.');
+
+  // 파일 없을시 PathNotFoundException: Cannot open file 에러 발생
+  final text = file.readAsStringSync();
   print(text);
 }
