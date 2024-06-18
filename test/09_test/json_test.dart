d7911487ce2d01077d2b5a9ee6f514da271876fe
lgy15 <lgy159@naver.com>
2024-06-19 02:36:05 +0900

    fix: 직렬화 재구현, test 추가

--- test/09_test/json_test.dart
+++ test/09_test/json_test.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:learn_dart_together/09_exception_file_data/json/department.dart';
 import 'package:learn_dart_together/09_exception_file_data/json/employee.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
@@ -10,5 +12,15 @@ void main() {
     final jsonString = '{"name":"총무부","leader":{"name":"홍길동","age":41}}';
 
     expect(companyFile.readAsStringSync(), jsonString);
+  });
+  
+  test('company 직렬화 테스트', () {
+    final leader = Employee(name: '리더', age: 40);
+    final dept = Department(name: '부서', leader: leader);
+
+    final serialization = dept.toJson();
+    final deserialization = Department.fromJson(serialization);
+
+    expect(deserialization, dept);
   });
 }
