eb37242a986942bd07ecdd6b26270d50872e2f0d
hugedreamhd <hugedreamhd@gmail.com>
2024-06-20 01:59:53 +0900

    fix : 240619 과제 1번하다가 너무 졸려서 일단 커밋..

--- lib/12_debugging/training.dart
+++ lib/12_debugging/training.dart
@@ -22,52 +22,63 @@ final String json = '''{
  ]
 }''';
 
-
-
-class CollectionChartDataList {
-  String collectionName;
-  List<CollectionSalePrice>? collectionSalePrice;
-
-  CollectionChartDataList({
-    required this.collectionName,
-    this.collectionSalePrice,
-  });
-
-
-
-
-  Map<String, dynamic> tojson() => {
-        'collectionName': collectionName,
-        'collectionSalePrice': collectionSalePrice?.tojson,
-      };
-
-
-  CollectionChartDataList.fromJson(Map<String, dynamic> json)
-      : collectionName = json['name'],
-        collectionSalePrice = CollectionSalePrice.fromJson(json['cvtDateTime']);
-}
-
 class CollectionSalePrice {
   double price;
-  DateTime cvtDateTime;
+  String cvtDateTime;
 
   CollectionSalePrice({
     required this.price,
     required this.cvtDateTime,
   });
 
-  Map<String, dynamic> tojson() => {
+  Map<String, dynamic> toJson() => {
+        //메서드
         'price': price,
-        'cvtDateTime': cvtDateTime.toIso8601String(),//DateTime을 직렬화로 바꿀때 씀
+        'cvtDateTime': cvtDateTime, //DateTime을 직렬화로 바꿀때 쓴데
       };
 
   CollectionSalePrice.fromJson(Map<String, dynamic> json)
-      : price = json['price'].toDouble,
+      : price = json['price'].toDouble(),
         cvtDateTime = json['cvtDateTime'];
 }
-void main() {
-  Map<String,dynamic> jsonString = jsonDecode(json);
-  List<dynamic> collection = jsonString['collectionChartDataList'];
-  collection.forEach((e) => print(e));
 
+class CollectionChartDataList {
+  String collectionName;
+  CollectionSalePrice collectionSalePrice;
+
+  CollectionChartDataList({
+    required this.collectionName,
+    required this.collectionSalePrice,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'collectionName': collectionName,
+        'collectionSalePrice': collectionSalePrice,
+      };
+
+  CollectionChartDataList.fromJson(Map<String, dynamic> json)
+      : collectionName = json['name'],
+        collectionSalePrice = CollectionSalePrice.fromJson(json['cvtDateTime']);
+}
+
+class DataList {
+  List<CollectionChartDataList> collectionChartDataList;
+
+  DataList(this.collectionChartDataList);
+
+  Map<String, dynamic> toJson() => {
+        'collectionChartDataList': collectionChartDataList,
+      };
+
+  DataList.fromJson(Map<String, dynamic> json)
+      : collectionChartDataList = (json['collectionChartDataList'] as List)
+  .map((e) => CollectionChartDataList.fromJson(e)).toList();
+}
+
+void main() {
+  // Map<String,dynamic> jsonString = jsonDecode(json);
+  // List<dynamic> collection = jsonString['collectionChartDataList'];
+  // collection.forEach((e) => print(e));
+  DataList list = DataList.fromJson(jsonDecode(json));
+  print(jsonEncode(list.toJson()));
 }
