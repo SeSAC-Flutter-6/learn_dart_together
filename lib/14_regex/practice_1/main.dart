a3ea5f7e34c4ddd001a5847ed5c56944fc8f30e6
mmmkb1 <mmmkb1@naver.com>
2024-06-19 20:58:29 +0900

    [refactor] 정규 표현식 과제1 코드 리팩토링

--- lib/14_regex/practice_1/main.dart
+++ lib/14_regex/practice_1/main.dart
@@ -1,7 +1,14 @@
 void main() {
   List<String> numsString = [];
-  String str = [for (int i = 1; i <= 100; i++) '$i'].join(',');
-  str = str.replaceAllMapped(RegExp(r'$'), (match) => ',');
+  String str = '';
+  int temp = 0;
+
+  for (var i = 1; i <= 100; i++) {
+    temp = i;
+    str += '$temp,';
+  }
+  print(str);
+
   numsString = str.split(',');
   print(numsString);
 }
