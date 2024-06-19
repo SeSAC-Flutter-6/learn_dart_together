4b5bfaca5772d84b44807ee64a5ee39e959587ef
Kimdahee <dh980502@gmail.com>
2024-06-20 02:44:43 +0900

    수업중 코드, 수정

--- lib/13_data_format/main.dart
+++ lib/13_data_format/main.dart
@@ -5,11 +5,15 @@ import 'package:learn_dart_together/13_data_format/department.dart';
 import 'package:learn_dart_together/13_data_format/employee.dart';
 
 void main() {
-  String jsonString = '{"name":"총무부","leader":{"name":"홍길동","age":41}}';
-  Map<String, dynamic> json = jsonDecode(jsonString);
-  print(Department.fromJson(json));
-  introduce(Department(name: '총무부', leader: Employee(name: '홍길동', age: 41)),
-      'lib/13_data_format/company.txt');
+  // String jsonString = '{"name":"총무부","leader":{"name":"홍길동","age":41}}';
+  // Map<String, dynamic> json = jsonDecode(jsonString);
+  // print(Department.fromJson(json));
+  // introduce(Department(name: '총무부', leader: Employee(name: '홍길동', age: 41)),
+  //     'lib/13_data_format/company.txt');
+
+  Employee employee = Employee(name: '홍길동', age: 41);
+  print(employee.toJson().toString());
+  print(jsonEncode(employee.toJson()));
 
   final map = {
     'name': '총무부',
