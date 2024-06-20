fd1b65eb64f6c9d6883a901ecbd1c0ee88b5ec48
Kimdahee <dh980502@gmail.com>
2024-06-20 21:02:04 +0900

    240620 비동기 연습문제

---
+++ lib/17_async/file_processing.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  changeFile('lib/17_async/sample.csv', 'lib/17_async/sample_copy.csv');
+}
+
+void changeFile(String path, String target) async {
+  try {
+    File source = File(path);
+    String text = await source.readAsString();
+    text = text.replaceAll('한석봉', '김석봉');
+    File result = File(target);
+    result.writeAsString(text);
+  } catch (e) {
+    print('파일을 찾을 수 없습니다: $e');
+  }
+}
