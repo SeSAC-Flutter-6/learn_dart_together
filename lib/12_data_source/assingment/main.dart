b5e8ef0019cd2047223ef6fb119de7b33418e2ef
lgy15 <lgy159@naver.com>
2024-06-21 17:43:58 +0900

    chore: 과제 제출 폴더 위치 변경

--- lib/12_data_source/assingment/main.dart
+++ lib/12_data_source/assingment/main.dart
@@ -4,16 +4,17 @@ import 'package:learn_dart_together/data_source/todo_data_source.dart';
 
 Future<void> main() async {
   final dataSource = TodoDataSource();
+  final savePath = 'lib/12_data_source/assingment';
 
   // assingment 1
   final id = 1;
   final todo = dataSource.getTodo(id);
-  final File assingment1 = File('12_data_source/assingment1');
+  final File assingment1 = File('$savePath/assingment1');
   await assingment1.writeAsString((await todo).toString());
 
   // assingment 2
   final todos = dataSource.getTodos();
-  final File assingment2 = File('12_data_source/assingment2');
+  final File assingment2 = File('$savePath/assingment2');
   await assingment2.writeAsString((await todos).toString());
 
   // assingment 3
