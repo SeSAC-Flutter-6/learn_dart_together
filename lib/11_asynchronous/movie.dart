891d4f67312580edd47a3c0bc2499a498bdd1f05
S-YUN <https://github.com/S-YUN>
2024-06-21 10:42:23 +0900

    4종세트 연습

--- lib/11_asynchronous/movie.dart
+++ lib/11_asynchronous/movie.dart
@@ -16,4 +16,41 @@ class Movie {
       releaseYear: json['year'],
     );
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'director': director,
+      'year': releaseYear,
+    };
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Movie &&
+          runtimeType == other.runtimeType &&
+          title == other.title &&
+          director == other.director &&
+          releaseYear == other.releaseYear;
+
+  @override
+  int get hashCode => Object.hash(title, director, releaseYear);
+
+  @override
+  String toString() {
+    return 'Movie{title: $title, director: $director, year: $releaseYear}';
+  }
+
+  Movie copyWith({
+    String? title,
+    String? director,
+    int? releaseYear,
+  }) {
+    return Movie(
+      title: title ?? this.title,
+      director: director ?? this.director,
+      releaseYear: releaseYear ?? this.releaseYear,
+    );
+  }
 }
