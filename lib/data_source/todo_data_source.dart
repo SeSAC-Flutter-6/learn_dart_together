b94de084aaf57a909c95fefdc241f6dfe9ababb3
lcw <skcjddn@gmail.com>
2024-06-21 19:35:34 +0900

    feat: home work

---
+++ lib/data_source/todo_data_source.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+void main() async {
+  //print(await TodoDatasource().getTodos());
+  print(await TodoDatasource().getTodo(id: 1));
+}
+
+class TodoDatasource {
+  final String _baseUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  Future<List<Todo>> getTodos() async {
+    final http.Response response = await http.get(Uri.parse(_baseUrl));
+
+    List jsonList = jsonDecode(response.body);
+    return jsonList.map((e) => Todo.fromJson(e)).toList();
+  }
+
+  Future<Todo> getTodo({required int id}) async {
+    final http.Response response = await http.get(Uri.parse('$_baseUrl/$id'));
+
+    final json = jsonDecode(response.body);
+    return Todo.fromJson(json);
+  }
+}
+
+class Todo {
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  Todo({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+
+  Todo.fromJson(Map<String, dynamic> json)
+      : userId = json['userId'],
+        id = json['id'],
+        title = json['title'],
+        completed = json['completed'];
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Todo &&
+          runtimeType == other.runtimeType &&
+          userId == other.userId &&
+          id == other.id &&
+          title == other.title &&
+          completed == other.completed;
+
+  @override
+  int get hashCode =>
+      userId.hashCode ^ id.hashCode ^ title.hashCode ^ completed.hashCode;
+
+  @override
+  String toString() {
+    return 'TodoDatasource{userId: $userId, id: $id, title: $title, completed: $completed}';
+  }
+
+  Todo copyWith({
+    int? userId,
+    int? id,
+    String? title,
+    bool? completed,
+  }) {
+    return Todo(
+      userId: userId ?? this.userId,
+      id: id ?? this.id,
+      title: title ?? this.title,
+      completed: completed ?? this.completed,
+    );
+  }
+}
