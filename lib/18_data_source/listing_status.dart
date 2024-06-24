14132034ff7f5421e26a45dced781ffac1d2be02
Kimdahee <dh980502@gmail.com>
2024-06-25 01:13:14 +0900

    fix : csv 연습문제 수정

--- lib/18_data_source/listing_status.dart
+++ lib/18_data_source/listing_status.dart
@@ -2,36 +2,33 @@ class ListingStatus {
   String symbol;
   String name;
 
-  // String exchange;
-  // String asserType;
-  // String ipoDate;
-  // String delistngDate;
-  // String status;
+  String exchange;
+  String asserType;
+  String ipoDate;
+  String delistngDate;
+  String status;
 
   ListingStatus({
     required this.symbol,
     required this.name,
-    // required this.exchange,
-    // required this.asserType,
-    // required this.ipoDate,
-    // required this.delistngDate,
-    // required this.status,
+    required this.exchange,
+    required this.asserType,
+    required this.ipoDate,
+    required this.delistngDate,
+    required this.status,
   });
 
   ListingStatus.fromCsv(List<String> csv)
-      : symbol = csv[0],
-        name = csv[2];
+      : symbol = csv.length > 6 ? csv[0] : '',
+        name = csv.length > 6 ? csv[1] : '',
+        exchange = csv.length > 6 ? csv[2] : '',
+        asserType = csv.length > 6 ? csv[3] : '',
+        ipoDate = csv.length > 6 ? csv[4] : '',
+        delistngDate = csv.length > 6 ? csv[5] : '',
+        status = csv.length > 6 ? csv[6] : '';
 
   @override
   String toString() {
-    return 'ListingStatus{symbol: $symbol, name: $name}';
+    return 'ListingStatus{symbol: $symbol, name: $name, exchange: $exchange, asserType: $asserType, ipoDate: $ipoDate, delistngDate: $delistngDate, status: $status}';
   }
-
-// name = csv[1] ?? '',
-// exchange = csv[2] ?? '',
-// asserType = csv[3] ?? '',
-// ipoDate = csv[4] ?? '',
-// delistngDate = csv[5] ?? '',
-// status = csv[6] ?? ''
-// ;
 }
--- lib/data_source/listing_status_date_source.dart
+++ lib/data_source/listing_status_date_source.dart
@@ -6,14 +6,13 @@ class ListingStatusDateSource {
     final response = await http.get(Uri.parse(
         'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=demo'));
     List<String> list = response.body.split('\n').skip(1).toList();
-    final listSplit = list.map((e) => e.split(',').map((e)=>e.trim()).toList()).toList();
+    final listSplit =
+        list.map((e) => e.split(',').map((e) => e.trim()).toList()).toList();
     return listSplit.map((e) => ListingStatus.fromCsv(e)).toList();
-    for(int i = 0; i<100; i ++){
-    print(listSplit[i]);}
   }
 }
 
 void main() async {
   final data = await ListingStatusDateSource().getListingStatus();
-  // print(data);
+  print(data);
 }
