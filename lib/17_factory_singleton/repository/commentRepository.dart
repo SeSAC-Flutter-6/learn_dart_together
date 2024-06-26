d87e3a84fd19b7a9b015454129e2cb4ec8a32f6c
Sangin Han <hugedreamhd@gmail.com>
2024-06-26 20:41:52 +0900

    fix : 26일 과제 1번 일단 한거 같은데...?

--- lib/17_factory_singleton/repository/commentRepository.dart
+++ lib/17_factory_singleton/repository/commentRepository.dart
@@ -2,6 +2,7 @@ import 'package:learn_dart_together/17_factory_singleton/model/comment_model.dar
 
 abstract interface class CommentRepository {
   //CRUD
+  //메뉴판
 
   Future<List<Comment>> getComments(int postId);
 }
--- lib/17_factory_singleton/repository/commentRepositoryImpl.dart
+++ lib/17_factory_singleton/repository/commentRepositoryImpl.dart
@@ -1,21 +1,12 @@
-import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:learn_dart_together/17_factory_singleton/model/comment_model.dart';
 import 'package:learn_dart_together/17_factory_singleton/repository/commentRepository.dart';
-
+import 'package:learn_dart_together/data_source/data_source.dart';
+//메뉴판
 class CommentRepositoryImpl implements CommentRepository {
-  final Comment commentRepository;
+  final CommentDataSource commentDataSource;
 
-  CommentRepositoryImpl(this.commentRepository);
+  CommentRepositoryImpl(this.commentDataSource);
 
-//직접 구현 해야 하니 여기서 파싱도 하고 그래야하나봐
   @override
-  Future<List<Comment>> getComments(int postId) async {
-    final response = await http.get(
-      Uri.parse('https://jsonplaceholder.typicode.com/comments/$postId'),
-    );
-
-    return [];
-  }
-}
+  Future<List<Comment>> getComments(int postId) => commentDataSource.getCommentsPostId(postId);
