a566e407d85b2f77ee49aca8abab27049d6b5110
S-YUN <candy9won@naver.com>
2024-06-28 02:48:08 +0900

    0627 과제 1번 완료

--- test/14_unit_test/repository/user_repository_test.dart
+++ test/14_unit_test/repository/user_repository_test.dart
@@ -5,126 +5,109 @@ import '../../dummy_data/dummy_user.dart';
 import 'mock_user_api.dart';
 
 void main() {
-  late MockUserApi _mockUserApi;
-  late UserRepositoryImpl _userRepository;
+  late MockUserApi mockUserApi;
+  late UserRepositoryImpl userRepository;
 
   setUp(() {
-    _mockUserApi = MockUserApi();
-    _userRepository = UserRepositoryImpl(userApi: _mockUserApi);
+    mockUserApi = MockUserApi();
+    userRepository = UserRepositoryImpl(userApi: mockUserApi);
+    dummyUsers.clear();
+    dummyUsers.addAll(initialDummyUsers);
   });
 
   group('createUser 테스트', () {
     test('유저 생성', () async {
-      final newUser = newbie;
-      final created = await _userRepository.createUser(newbie);
-
+      User user = User.fromJson(newbie);
+      final created = await userRepository.createUser(user);
       expect(created, true);
-
-      // Verify that user was added
-      final users = await _userRepository.getAllUsers();
-      expect(users.any((user) => user.id == newUser.id), true);
     });
 
     test('이미 존재하는 유저는 생성 불가', () async {
-      final existingUser = User(id: 1, name: 'John', email: 'john@example.com');
-      final created = await _userRepository.createUser(existingUser);
-
+      final existingUser = User.fromJson(user1);
+      final created = await userRepository.createUser(existingUser);
       expect(created, false);
     });
   });
 
   group('getUser 테스트', () {
     test('유저가 존재한다면 유저를 반환', () async {
-      final userId = 1;
-      final user = await _userRepository.getUser(userId);
-
-      expect(user!.id, userId);
+      final existingUser = User.fromJson(user1);
+      final user = await userRepository.getUser(existingUser.id);
+      expect(existingUser, user);
     });
 
     test('유저가 존재하지 않는다면 null 반환', () async {
-      final userId = 10; // Assuming userId 10 does not exist in mock data
-      final user = await _userRepository.getUser(userId);
-
+      User nonExistentUser = User.fromJson(newbie);
+      final user = await userRepository.getUser(nonExistentUser.id);
       expect(user, null);
     });
   });
 
   group('updateUser 테스트', () {
     test('존재하지 않는 유저라면 false', () async {
-      final nonExistingUser =
-          User(id: 10, name: 'Tom', email: 'tom@example.com');
-      final updated = await _userRepository.updateUser(nonExistingUser);
-
+      User nonExistentUser = User.fromJson(newbie);
+      final updated = await userRepository.updateUser(nonExistentUser);
       expect(updated, false);
     });
 
     test('존재하는 유저지만 기존 정보와 달라진 정보가 없다면 false', () async {
-      final existingUser = User(id: 1, name: 'John', email: 'john@example.com');
-      final updated = await _userRepository.updateUser(existingUser);
-
+      final existingUser = User.fromJson(user1);
+      final updated = await userRepository.updateUser(existingUser);
       expect(updated, false);
     });
 
     test('존재하는 유저이며 달라진 정보가 있다면 업데이트 후 true', () async {
-      final existingUser = User(id: 1, name: 'John', email: 'john@example.com');
-      final updatedUser = existingUser.copyWith(name: 'Johnny');
-      final updated = await _userRepository.updateUser(updatedUser);
-
+      final existingUser = User.fromJson(user1);
+      final String newEmail = 'new_email@example.com';
+      final updatedUser = existingUser.copyWith(email: newEmail);
+      final updated = await userRepository.updateUser(updatedUser);
       expect(updated, true);
 
-      // Verify that user was updated
-      final user = await _userRepository.getUser(existingUser.id);
-      expect(user!.name, 'Johnny');
+      final user = await userRepository.getUser(updatedUser.id);
+      expect(user!.email, newEmail);
     });
   });
 
   group('deleteUser 테스트', () {
     test('존재하지 않는 유저라면 false', () async {
-      final userId = 10; // Assuming userId 10 does not exist in mock data
-      final deleted = await _userRepository.deleteUser(userId);
-
+      User nonExistentUser = User.fromJson(newbie);
+      final deleted = await userRepository.deleteUser(nonExistentUser.id);
       expect(deleted, false);
     });
 
     test('존재하는 유저면 삭제 후 true', () async {
-      final userId = 1;
-      final deleted = await _userRepository.deleteUser(userId);
-
+      final existingUser = User.fromJson(user1);
+      final deleted = await userRepository.deleteUser(existingUser.id);
       expect(deleted, true);
 
-      // Verify that user was deleted
-      final user = await _userRepository.getUser(userId);
-      expect(user, null);
+      final deletedUser = await userRepository.getUser(existingUser.id);
+      expect(deletedUser, null);
     });
   });
 
   group('getAllUsers 테스트', () {
     test('모든 유저를 가져옴', () async {
-      final users = await _userRepository.getAllUsers();
-
-      expect(users.length, _mockUserApi.dummyUsers.length);
+      final users = await userRepository.getAllUsers();
+      expect(users.length, dummyUsers.length);
     });
 
     test('유저가 하나도 존재하지 않는다면 빈 리스트 반환', () async {
-      _mockUserApi.dummyUsers.clear(); // Clear dummy users for this test
-      final users = await _userRepository.getAllUsers();
-
+      dummyUsers.clear();
+      final users = await userRepository.getAllUsers();
       expect(users.length, 0);
     });
   });
 
   group('findUserByEmail 테스트', () {
     test('이메일과 일치하는 유저를 반환', () async {
-      final email = 'alice@example.com';
-      final user = await _userRepository.findUserByEmail(email);
-
-      expect(user!.email, email);
+      final existingUser = User.fromJson(user1);
+      final user = await userRepository.findUserByEmail(existingUser.email);
+      expect(user!.email, existingUser.email);
     });
 
     test('일치하는 이메일이 없다면 null 반환', () async {
-      final email = 'unknown@example.com'; // Assuming this email does not exist
-      final user = await _userRepository.findUserByEmail(email);
-
+      User nonExistentUser = User.fromJson(newbie);
+      final user = await userRepository.findUserByEmail(nonExistentUser.email);
       expect(user, null);
     });
   });
--- test/dummy_data/dummy_user.dart
+++ test/dummy_data/dummy_user.dart
@@ -1,35 +1,36 @@
-List<Map<String, dynamic>> dummyUsers = [
-  {
-    "id": 1,
-    "name": "John",
-    "email": "john@example.com",
-    "createdAt": "2023-06-24T10:00:00.000Z"
-  },
-  {
-    "id": 2,
-    "name": "Bob",
-    "email": "bob@example.com",
-    "createdAt": "2023-06-23T11:30:00.000Z"
-  },
-  {
-    "id": 3,
-    "name": "Alice",
-    "email": "alice@example.com",
-    "createdAt": "2023-06-21T13:15:00.000Z"
-  },
-  {
-    "id": 4,
-    "name": "Cloe",
-    "email": "cloe@example.com",
-    "createdAt": "2023-06-20T13:15:00.000Z"
-  },
-  {
-    "id": 5,
-    "name": "Denny",
-    "email": "denny@example.com",
-    "createdAt": "2023-06-22T13:15:00.000Z"
-  }
-];
+Map<String, dynamic> user1 = {
+  "id": 1,
+  "name": "John",
+  "email": "john@example.com",
+  "createdAt": "2023-06-24T10:00:00.000Z"
+};
+
+Map<String, dynamic> user2 = {
+  "id": 2,
+  "name": "Bob",
+  "email": "bob@example.com",
+  "createdAt": "2023-06-23T11:30:00.000Z"
+};
+
+Map<String, dynamic> user3 = {
+  "id": 3,
+  "name": "Alice",
+  "email": "alice@example.com",
+  "createdAt": "2023-06-21T13:15:00.000Z"
+};
+
+Map<String, dynamic> user4 = {
+  "id": 4,
+  "name": "Cloe",
+  "email": "cloe@example.com",
+  "createdAt": "2023-06-20T13:15:00.000Z"
+};
+Map<String, dynamic> user5 = {
+  "id": 5,
+  "name": "Denny",
+  "email": "denny@example.com",
+  "createdAt": "2023-06-22T13:15:00.000Z"
+};
 
 Map<String, dynamic> newbie = {
   "id": 100,
@@ -37,3 +38,12 @@ Map<String, dynamic> newbie = {
   "email": "neo@example.com",
   "createdAt": "2023-06-27T13:15:00.000Z"
 };
+
+List<Map<String, dynamic>> dummyUsers = [];
+List<Map<String, dynamic>> initialDummyUsers = [
+  user1,
+  user2,
+  user3,
+  user4,
+  user5
+];
