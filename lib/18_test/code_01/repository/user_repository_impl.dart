d097f51ce7d75faac0fe60947b1b749286609014
Sangin Han <hugedreamhd@gmail.com>
2024-06-29 00:03:38 +0900

    feat : Dto_Mapper 과제 2_pair_coding

--- lib/18_test/code_01/repository/user_repository_impl.dart
+++ lib/18_test/code_01/repository/user_repository_impl.dart
@@ -3,33 +3,5 @@ import 'package:learn_dart_together/18_test/code_01/model/user.dart';
 import 'package:learn_dart_together/18_test/code_01/repository/user_repository.dart';
 
 class UserRepositoryImpl implements UserRepository {
-  @override
-  Future<User> getUser() async {
-    return User(name: 'user', id: null, email: '', created_At: null);
-  }
 
-  @override
-  Future<User> createUser(Object user) {
-
-    }
-
-  @override
-  Future<void> deleteUser(int id) {
-     }
-
-  @override
-  Future<List<User>> fetchAllUsers() {
-    }
-
-  @override
-  Future<User> fetchUser(int id) {
-     }
-
-  @override
-  Future<User> findUserByEmail(String email) {
-     }
-
-  @override
-  Future<User> updateUser(User user) {
-     }
 }
