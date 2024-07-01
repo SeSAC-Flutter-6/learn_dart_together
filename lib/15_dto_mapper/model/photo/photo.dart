6470b386a1cad73bd8833c8a1ae9d6feeeb31434
S-YUN <candy9won@naver.com>
2024-07-01 16:55:31 +0900

    feat: photo model 설계

---
+++ lib/15_dto_mapper/model/photo/photo.dart
@@ -0,0 +1,47 @@
+class Photo {
+  int id;
+  PhotoType type;
+  DateTime createdAt;
+  String url;
+  Photo({
+    required this.id,
+    required this.type,
+    required this.createdAt,
+    required this.url,
+  });
+
+  Photo copyWith({
+    int? id,
+    PhotoType? type,
+    DateTime? createdAt,
+    String? url,
+  }) {
+    return Photo(
+      id: id ?? this.id,
+      type: type ?? this.type,
+      createdAt: createdAt ?? this.createdAt,
+      url: url ?? this.url,
+    );
+  }
+
+  @override
+  String toString() =>
+      'Photo(id: $id, type:$type createdAt: $createdAt, url: $url)';
+
+  @override
+  bool operator ==(covariant Photo other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id && other.url == url;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ url.hashCode;
+}
+
+enum PhotoType {
+  article,
+  image,
+  video,
+  unknown,
+}
