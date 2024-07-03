0bde0f2ef7fc35e61638aaa578ffd38ffcab4d34
lcw <skcjddn@gmail.com>
2024-07-04 01:18:02 +0900

    feat: Add 과제5

--- lib/18_dto_mapper/data_source/user_data_source_impl.dart
+++ lib/18_dto_mapper/data_source/user_data_source_impl.dart
@@ -5,12 +5,22 @@ import 'package:learn_dart_together/18_dto_mapper/dto/user_dto.dart';
 import 'package:http/http.dart' as http;
 
 class UserDataSourceImpl implements UserDataSource {
+  final String _baseUrl;
+
+  UserDataSourceImpl({String? baseUrl})
+      : _baseUrl = baseUrl ?? 'https://jsonplaceholder.typicode.com/users';
+
   @override
   Future<List<UserDto>> getUsers() async {
-    final http.Response response =
-        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    final http.Response response = await http.get(Uri.parse(_baseUrl));
 
     final List jsonList = jsonDecode(response.body);
     return jsonList.map((e) => UserDto.fromJson(e)).toList();
+  }
+
+  @override
+  Future<UserDto> getUser(int id) async {
+    final http.Response response = await http.get(Uri.parse('$_baseUrl/$id'));
+    return UserDto.fromJson(jsonDecode(response.body));
   }
 }
