b3bf71d9066667988a9a67451b4478c8cf96228b
Jayone Lee <kenbugless@gmail.com>
2024-07-04 02:47:57 +0900

    [update]:Feat Assignment on network

--- lib/21_network/repository/movie_repository.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:learn_dart_together/240329/model/movie.dart';
-
-// abstract interface class MovieRepository {
-//   Future<List<Movie>> getMovieInfoList();
-//   Future<Movie> getMovie(int id);
-// }
