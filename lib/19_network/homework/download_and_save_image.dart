a0ef084c58f4b5d71d681147f184d0018abfbb05
lcw <skcjddn@gmail.com>
2024-07-04 01:17:42 +0900

    feat: Add 과제2

---
+++ lib/19_network/homework/download_and_save_image.dart
@@ -0,0 +1,25 @@
+import 'dart:typed_data';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+
+class DownloadAndSaveImage {
+  Future<Uint8List> downloadImage(String url) async {
+    final http.Response response =
+        await http.get(Uri.parse('https://alimipro.com/favicon.ico'));
+    return response.bodyBytes;
+  }
+
+  Future<File> saveFile(Uint8List bytes, String filename) async {
+    final File file = File(filename);
+    await file.writeAsBytes(bytes);
+    return file;
+  }
+}
+
+void main() async {
+  DownloadAndSaveImage downloadAndSaveImage = DownloadAndSaveImage();
+  Uint8List imageData = await downloadAndSaveImage
+      .downloadImage('https://alimipro.com/favicon.ico');
+
+  await downloadAndSaveImage.saveFile(imageData, './lib/19_network/icon.ico');
+}
