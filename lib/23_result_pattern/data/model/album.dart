4be928acb9c38abfc7a50594360cc51d75f60b60
Kimdahee <dh980502@gmail.com>
2024-07-05 01:09:54 +0900

    240704 수업중 코드

---
+++ lib/23_result_pattern/data/model/album.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'album.freezed.dart';
+
+part 'album.g.dart';
+
+@freezed
+class Album with _$Album {
+  const factory Album({
+    required int id,
+    required int userId,
+    required String title,
+  }) = _Album;
+
+  factory Album.fromJson(Map<String, Object?> json) => _$AlbumFromJson(json);
+}
