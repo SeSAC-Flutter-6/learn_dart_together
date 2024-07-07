d466dbcda018dd132b6b3b7b669d97ebb2d8ed62
Kimdahee <dh980502@gmail.com>
2024-07-08 00:20:06 +0900

    fix : user model 수정

--- lib/24_architecture/book_management_program/model/user.dart
+++ lib/24_architecture/book_management_program/model/user.dart
@@ -10,13 +10,24 @@ class User with _$User {
   const User._();
   const factory User({
     required int id,
-    required int registrationDate,
+    required String registrationDate,
     required String name,
     required String address,
     required String phoneNumber,
-    required DateTime birthday,
+    required String birthday,
   }) = _User;
 
   factory User.fromJson(Map<String, Object?> json) => _$UserFromJson(json);
-  int get age => DateTime.now().year - birthday.year;
+  int get age => DateTime.now().year - int.parse(birthday.split('/')[0]);
+
+  factory User.fromList(List<dynamic> list) {
+    return User(
+      id: list[0],
+      registrationDate: list[1],
+      name: list[2],
+      address: list[3],
+      phoneNumber: list[4],
+      birthday: list[5],
+    );
+  }
 }
